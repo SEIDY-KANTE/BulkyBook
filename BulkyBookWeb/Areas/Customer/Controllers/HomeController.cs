e33f3d859e3eef67d64c757c582ad6f6144064f5
Seidy KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-12-06 10:58:46 +0300

    HomeController implementation

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
         public IActionResult Index()
         {
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
-
+            
             return View(productList);
         }
 
