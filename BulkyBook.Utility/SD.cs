676b77a8f67aa195086a611f145b55b97b3cebce
Seidy KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2022-12-07 15:50:11 +0300

    Register

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -9,5 +9,9 @@ namespace BulkyBook.Utility
     //We don't have to create an object of this class, and here it will hold all the Static Details (SD)
     public static class SD
     {
+        public const string Role_User_Indi = "Individual";
+        public const string Role_User_Comp = "Company";
+        public const string Role_User_Admin = "Admin";
+        public const string Role_User_Employee = "Employee";
     }
 }
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,23 +1,26 @@
 using BulkyBook.DataAccess;
 using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-
+//builder.Services.AddDefaultIdentity<IdentityUser>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
